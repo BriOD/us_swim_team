5600ea1af3d2722c59a8b7261702d6ad8ad0d670
BriOD <briodonoghue@gmail.com>
2016-08-15 12:21:29 -0400

    created call method, and basic framework for menu method

--- lib/us_swim_team/cli.rb
+++ lib/us_swim_team/cli.rb
@@ -4,9 +4,22 @@ class UsSwimTeam::CLI
 
   def call
     puts "Welcome to the United States 2016 Olympic swim team!!!"
+    menu
   end
-  
 
+  def menu
+    puts "Enter 'm' for a list of the mens team. Enter 'w' for a list of the womens team. Enter 'exit' to exit."
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input 
+      when "m"
+        puts "mens list"  #placeholder for actual list to come later
+      when "w"
+        puts "womens list" #placeholder for actual list to come late
+      end
+    end
+  end
 
 
 end
